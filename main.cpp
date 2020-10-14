aed7acaaaace0014c09045ee94b2dddad0542e20
phdfreitas <pedro.hnq.fraga@gmail.com>
2020-10-14 15:28:43 -0300

    Comentários

--- main.cpp
+++ main.cpp
@@ -19,9 +19,11 @@ using namespace std;
 	bool exec = true;
 	int music = 0;
 	int playlistControl = 0;
-	char numero;
 // Fim
 
+
+// Função para usar a PThread
+// Basicamente nessa 1ª entrega só foi usada para imprimir a playlist
 void *printPlaylist(void *arg){
 
 	char * atual = (char *)arg;
@@ -32,13 +34,14 @@ void *printPlaylist(void *arg){
 
 int main(int argc, char const *argv[]){
 	
-	string playlist[1000];
-
+	string playlist[1000]; // Irá guardar o nome/caminho das músicas. 
+						   // Será realmente a nossa playlist	
 	initscr();
 
 	printw("Você deve digitar o caminho para adicionar uma música.\n(Ex: /home/user/Music/musica.wav).\n");
 	printw("Para parar de adicionar músicas a fila de reprodução, digite 0.\n");
 	
+	// Enquanto o usuário não digitar "0", ele pode continuar adicionando músicas na playlist.
 	while(true){
 		char musica[100];
 		getstr(musica);
@@ -49,35 +52,41 @@ int main(int argc, char const *argv[]){
 		music++;
 	}
 
-	printw("\n----------------------------------------------------\n"); // Representa o "Menu"
+	// Simula o menu de opções para o usuário.
+	printw("\n--------------------------------------------------------------------\n"); // Representa o "Menu"
 		printw("P Play | N Próxima | E Sair | R Remover uma música da playlist."); // Representa o "Menu"
-	printw("\n----------------------------------------------------\n");
+	printw("\n--------------------------------------------------------------------\n");
 	
+	// Declaro um array de Thread com a quantidade total de músicas + 1 
 	pthread_t threads[music + 1];
 
 	printw("\nSua playlist\n");
 	
+	// Imprime para o usuário a playlist que ele criou
+	// Como dito, a PThread foi usuada aqui, mas provavelmente
+	// nas pŕóximas versões faremos um uso mais adequado.
 	for (int i = 0; i < music; i++){
-		int size = playlist[i].length();
-		char musicaAtual[size + 1];
+		int size = playlist[i].length(); // Pega o tamanho da música atual
+		char musicaAtual[size + 1]; // Cria um array de char com esse tamanho
 
-		strcpy(musicaAtual, playlist[i].c_str());
+		strcpy(musicaAtual, playlist[i].c_str()); // Copia o nome/caminho da música no array
 
+		// Chamamos a pthread_create passando o array de char como parâmetro
 		pthread_create(&threads[i], NULL, printPlaylist, musicaAtual);
 		pthread_join(threads[i], NULL);
 	}
 
-
+	// Basicamente, enquanto for "True", leia chamadas do usuário
 	while(exec){
 		bool atual = true; // Controla o tempo de execução da música atual
 
-		int entrada = getch(); // Guarda o valor digita pelo usuário
+		int entrada = getch(); // Guarda o valor digitado pelo usuário
 		switch(entrada){
 			case 'P':
 				playlistControl = music;
 				music = 0;
 
-				SDL_Init(SDL_INIT_EVERYTHING); // Inicia a biblioteca p/ o áudio
+				SDL_Init(SDL_INIT_EVERYTHING); // Inicia a biblioteca de áudio
 		  	
 				Mix_OpenAudio(frequency, format, channel, buffer);
 
@@ -86,14 +95,17 @@ int main(int argc, char const *argv[]){
 
 					Mix_PlayChannel(-1, song, 0); // E configura para reproduzir apenas uma vez
 
-					int next = getch();
+					int next = getch(); // Usado para caso o usuário queira pula a música atual.
+
 					Mix_FreeChunk(song); // "Libera" a música atual
 					music++; // vai para a próxima música
 				}
 		  		printw("\nSua playlist terminou. Se quiser recomeçar essa, pressione P.\nE se quiser sair, pressione E.");
 				break;
 
-			case 'R':
+			case 'R': // Caso o usuário queira remover uma música da playlist
+				// Imprimimos a playlist que ele criou
+
 				printw("\nSua playlist\n");
 				for (int i = 0; i < music; i++){
 					int size = playlist[i].length();
@@ -107,18 +119,19 @@ int main(int argc, char const *argv[]){
 					}
 				}
 
-				printw("\nDigite o número da música que você deseja deletar:\n");
+				printw("\nDigite o nome/caminho da música que você deseja deletar:\n");
 				
 				char musicaDelete[100];
-				getstr(musicaDelete);
+				getstr(musicaDelete); // Lê qual música ele quer excluir
 
-				for (int i = 0; i < music; i++){
+				// Exclui a música da playlist
+				for (int i = 0; i < music; i++){ 
 					if(musicaDelete == playlist[i]){
 						playlist[i] = "";
 					}
 				}
 				
-
+				// Mostra ao usuário como está a playlist dele depois da exclusão.
 				printw("\nSua playlist agora é:\n");
 
 				for (int i = 0; i < music; i++){
@@ -133,7 +146,7 @@ int main(int argc, char const *argv[]){
 					}
 				}
 				break;
-		  	case 'E':
+		  	case 'E': // Para sair do programa.
 		  	printw("Bye bye!\n");
 		  	exec = false;
 	    	refresh();

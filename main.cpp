298b10cfc2182406c7fdc5c0b7aadbe50001ffe4
phdfreitas <pedro.hnq.fraga@gmail.com>
2020-10-25 08:59:44 -0300

    Adicionando diretório

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <string>
+
 #include <ncurses.h>
+#include <pthread.h>
+
 #include <SDL/SDL.h>
 #include <SDL/SDL_mixer.h>
-#include <pthread.h>
+
+#include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
 
 using namespace std;
 
@@ -21,7 +27,6 @@ using namespace std;
 	int playlistControl = 0;
 // Fim
 
-
 // Função para usar a PThread
 // Basicamente nessa 1ª entrega só foi usada para imprimir a playlist
 void *printPlaylist(void *arg){
@@ -34,48 +39,78 @@ void *printPlaylist(void *arg){
 
 int main(int argc, char const *argv[]){
 	
-	string playlist[1000]; // Irá guardar o nome/caminho das músicas. 
-						   // Será realmente a nossa playlist	
+	char playlist[1000][1000]; // Irá guardar o nome/caminho das músicas. 
+						   // Será realmente a nossa playlist
+
+	char nomeMusicas[1000][1000];	
+	
+	DIR *dir;
+	struct dirent *sdirent;
+	
+	
 	initscr();
 
-	printw("Você deve digitar o caminho para adicionar uma música.\n(Ex: /home/user/Music/musica.wav).\n");
-	printw("Para parar de adicionar músicas a fila de reprodução, digite 0.\n");
+	int numLinhas, numColunas, y0, x0, y1, x1, y2, x2, y3, x3;
+
+    getmaxyx(stdscr, numLinhas, numColunas);
+
+    y0 = 0, x0 = 0;
+    y1 = numLinhas, x1 = numColunas;
+
+    WINDOW *win1 = newwin(y1, x1, y0, x0);
+    refresh();
+
+	string tip1 = "Você deve digitar o caminho para adicionar uma música.\n";
+	string tip2 = "(Ex: /home/user/Music/)."; 
+
+	mvwprintw(win1, 1, 0, tip1.c_str());
+	mvwprintw(win1, 2, 15, tip2.c_str());
 	
-	// Enquanto o usuário não digitar "0", ele pode continuar adicionando músicas na playlist.
-	while(true){
-		char musica[100];
-		getstr(musica);
-		
-		if(strcmp(musica, "0") == 0) break;
-		
-		playlist[music] = musica;
-		music++;
+	wrefresh(win1);
+	move(5, 0);
+
+	printw("> ");
+	char diretorio[100];
+	getstr(diretorio);
+
+	dir = opendir(diretorio);
+
+	if(dir == NULL){
+		printw("\nEsse diretório está vazio");
+		exit(1);
 	}
 
-	// Simula o menu de opções para o usuário.
-	printw("\n--------------------------------------------------------------------\n"); // Representa o "Menu"
-		printw("P Play | N Próxima | E Sair | R Remover uma música da playlist."); // Representa o "Menu"
-	printw("\n--------------------------------------------------------------------\n");
-	
-	// Declaro um array de Thread com a quantidade total de músicas + 1 
-	pthread_t threads[music + 1];
+	// Adicionando músicas do diretório na playlist 
+	char aux[100];
+	strcpy(aux, diretorio);
+	while((sdirent=readdir(dir)) != NULL){
+		
+		strcpy(diretorio, aux);
+		string str = sdirent -> d_name;
 
-	printw("\nSua playlist\n");
-	
-	// Imprime para o usuário a playlist que ele criou
-	// Como dito, a PThread foi usuada aqui, mas provavelmente
-	// nas pŕóximas versões faremos um uso mais adequado.
+		int size = str.length();
+		char musicaAtual[size + 1];
+
+		int index = str.find(".wav");
+
+		if((str.find(".wav") != -1)){
+			strcat(diretorio, str.c_str());
+			strcpy(playlist[music], diretorio);
+			str.erase(index, str.length() - 1);
+
+			strcpy(nomeMusicas[music], str.c_str());
+			music++;
+		}
+	}
+	closedir(dir);
+
+	printw("=-=-=-=-= Sua Playlist =-=-=-=-=-=\n");
 	for (int i = 0; i < music; i++){
-		int size = playlist[i].length(); // Pega o tamanho da música atual
-		char musicaAtual[size + 1]; // Cria um array de char com esse tamanho
-
-		strcpy(musicaAtual, playlist[i].c_str()); // Copia o nome/caminho da música no array
-
-		// Chamamos a pthread_create passando o array de char como parâmetro
-		pthread_create(&threads[i], NULL, printPlaylist, musicaAtual);
-		pthread_join(threads[i], NULL);
+		printw("%d - %s\n", (i+1), nomeMusicas[i]);
 	}
-
+	
+	refresh(); 
+	
 	// Basicamente, enquanto for "True", leia chamadas do usuário
 	while(exec){
 		bool atual = true; // Controla o tempo de execução da música atual
@@ -91,7 +126,7 @@ int main(int argc, char const *argv[]){
 				Mix_OpenAudio(frequency, format, channel, buffer);
 
 				while(music < playlistControl){ // Enquanto houver música na fila de reprodução
-					song = Mix_LoadWAV(playlist[music].c_str()); // Pega a música atual
+					song = Mix_LoadWAV(playlist[music]); // Pega a música atual
 
 					Mix_PlayChannel(-1, song, 0); // E configura para reproduzir apenas uma vez
 
@@ -101,50 +136,6 @@ int main(int argc, char const *argv[]){
 					music++; // vai para a próxima música
 				}
 		  		printw("\nSua playlist terminou. Se quiser recomeçar essa, pressione P.\nE se quiser sair, pressione E.");
-				break;
-
-			case 'R': // Caso o usuário queira remover uma música da playlist
-				// Imprimimos a playlist que ele criou
-
-				printw("\nSua playlist\n");
-				for (int i = 0; i < music; i++){
-					int size = playlist[i].length();
-					if(size > 0){
-						char musicaAtual[size + 1];
-
-						strcpy(musicaAtual, playlist[i].c_str());
-
-						pthread_create(&threads[i], NULL, printPlaylist, musicaAtual);
-						pthread_join(threads[i], NULL);
-					}
-				}
-
-				printw("\nDigite o nome/caminho da música que você deseja deletar:\n");
-				
-				char musicaDelete[100];
-				getstr(musicaDelete); // Lê qual música ele quer excluir
-
-				// Exclui a música da playlist
-				for (int i = 0; i < music; i++){ 
-					if(musicaDelete == playlist[i]){
-						playlist[i] = "";
-					}
-				}
-				
-				// Mostra ao usuário como está a playlist dele depois da exclusão.
-				printw("\nSua playlist agora é:\n");
-
-				for (int i = 0; i < music; i++){
-					int size = playlist[i].length();
-					if(size > 0){
-						char musicaAtual[size + 1];
-
-						strcpy(musicaAtual, playlist[i].c_str());
-
-						pthread_create(&threads[i], NULL, printPlaylist, musicaAtual);
-						pthread_join(threads[i], NULL);
-					}
-				}
 				break;
 		  	case 'E': // Para sair do programa.
 		  	printw("Bye bye!\n");
